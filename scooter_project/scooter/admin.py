95e26d283ea03866a23dd2b5deab40f7e285b55a
Maple <autumn2025718891@outlook.com>
2026-03-04 00:02:27 +0800

    Revert "Update Django admin registrations in scooter_project/scooter/admin.py Adjust model definitions in scooter_project/scooter/models.py"
    This reverts commit a2acfc2d3659420b68057f4f6168713138e641a8.

--- scooter_project/scooter/admin.py
+++ scooter_project/scooter/admin.py
@@ -1,5 +1,3 @@
 from django.contrib import admin
-from .models import Scooter, Order
 
-admin.site.register(Scooter)
-admin.site.register(Order)
+# Register your models here.
--- scooter_project/scooter/models.py
+++ scooter_project/scooter/models.py
@@ -1,27 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
-class Scooter(models.Model):
-    name = models.CharField(max_length=50, verbose_name="编号")
-    price = models.FloatField(verbose_name="每小时租赁价格")
-    is_available = models.BooleanField(default=True, verbose_name="是否可用")
-
-    def __str__(self):
-        return self.name
-    
-    class Meta:
-        verbose_name = "滑板车"
-        verbose_name_plural = "滑板车"
-
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="下单用户")
-    scooter = models.ForeignKey(Scooter, on_delete=models.CASCADE, verbose_name="租赁滑板车")
-    order_time = models.DateTimeField(auto_now_add=True, verbose_name="下单时间")
-    is_paid = models.BooleanField(default=False, verbose_name="是否支付")
-
-    def __str__(self):
-        return f"{self.user.username} - {self.scooter.name} - {self.order_time}"
-    
-    class Meta:
-        verbose_name = "订单"
-        verbose_name_plural = "订单"
+# Create your models here.
